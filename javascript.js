293d80ddd50c2ae59aacdf30c932d1d57efbf3b6
connectingtoaditya <aditygupta7033999066@gmail.com>
2025-07-28 23:27:59 +0530

    my first commit

---
+++ javascript.js
@@ -0,0 +1,7 @@
+
+    // alert("do not change anything");
+
+    age = 17;
+    if (age > 49) {console.log("strictly warn");}
+  else if (age >= 18) {console.log("allowed");}
+else {console.log("not allowed");}
